f43d736b9d84792d09ad33e02da143cda6c0289d
Remington Stone <remstone7@gmail.com>
2017-02-04 13:46:25 -0500

    assignment 3 add ui-sref and collection view

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -19,6 +19,10 @@
          .state('album', {
              url: '/album',
              templateUrl: '/templates/album.html'
+         })
+         .state('collection',{
+             url: '/collection',
+             templateUrl: '/templates/collection.html'
          });
      }
     
