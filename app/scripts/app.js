5508ba5670571c19303a70ec32bfa47a7712c8dd
Remington Stone <remstone7@gmail.com>
2017-02-04 12:43:29 -0500

    ch3 add routes and states

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,29 @@
-//define module
-angular.module('blocJams', []);
+(function() {
+    //configure paths
+     function config($stateProvider, $locationProvider) {
+         
+         // Configure the app's path
+         $locationProvider
+            .html5Mode({
+                // Disables #! routes
+                enabled: true,
+                // helps avoids $location error
+                requireBase: false
+         });
+         // Setup the template routes
+         $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+         })
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         });
+     }
+    
+    // set up Module
+     angular
+         .module('blocJams', ['ui.router'])
+         .config(config);
+ })();
